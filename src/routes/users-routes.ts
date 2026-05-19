cc2a6e9f7e248f7481be66248147bcf149b184c9
Lucas-Fabri <dev.cunhaww@gmail.com>
2026-05-19 13:30:02 -0300

    feat: add implement questions and users routes

---
+++ src/routes/users-routes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express'
+import { UsersController } from '../controllers/users-controller'
+
+export const usersRoutes = Router()
+
+const usersController = new UsersController()
+
+usersRoutes.get('/', usersController.index)
+usersRoutes.get('/:id', usersController.show)
+usersRoutes.post('/', usersController.create)
