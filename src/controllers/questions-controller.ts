eb333f0207a605756949462fb3d31596f3ab0798
Lucas-Fabri <dev.cunhaww@gmail.com>
2026-05-22 20:20:38 -0300

    feat: implement index, create, update, and remove methods in QuestionsController

--- src/controllers/questions-controller.ts
+++ src/controllers/questions-controller.ts
@@ -1,19 +1,62 @@
 import { Request, Response } from 'express';
+import { prisma } from '@/prisma';
 
 export class QuestionsController {
-  async index(_req: Request, res: Response) {
-    return res.json();
+  async index(req: Request, res: Response) {
+    const questions = await prisma.question.findMany({
+      where: {
+        title: {
+          contains: req.query.title?.toString().trim(),
+          mode: 'insensitive',
+        },
+      },
+      orderBy: {
+        title: 'asc',
+      },
+    });
+
+    return res.json(questions);
   }
 
   async create(req: Request, res: Response) {
+    const { title, content, user_id } = req.body;
+
+    await prisma.question.create({
+      data: {
+        title,
+        content,
+        userId: user_id,
+      },
+    });
+
     return res.status(201).json();
   }
 
   async update(req: Request, res: Response) {
+    const { id } = req.params as { id: string };
+    const { title, content } = req.body;
+
+    await prisma.question.update({
+      data: {
+        title,
+        content,
+      },
+      where: {
+        id,
+      },
+    });
+
     return res.json();
   }
 
   async remove(req: Request, res: Response) {
+    const { id } = req.params as { id: string };
+
+    await prisma.question.delete({
+      where: {
+        id,
+      },
+    });
     return res.status(204).send();
   }
 }
